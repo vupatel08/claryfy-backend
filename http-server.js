c9543675c7ec930cb6e75f5a36ab403e886fa271
Vraj Patel <vupatel08@gmail.com>
2025-06-22 21:32:24 -0400

    fix: use PORT environment variable for Heroku deployment

--- http-server.js
+++ http-server.js
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Whitelist of allowed origins
 const allowedOrigins = [
